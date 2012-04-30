c35c8404299a959b2bab4916d5b7a808b3e44022
Filipe Guth <lfguth@gmail.com>
2012-04-30 16:59:47 -0300

    event route created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Eventos::Application.routes.draw do
   match '/auth/:provider/callback' => 'authentications#create'
   
   root :to => 'pages#index'
+  
+  get "event" => "pages#event", :as => "event"
 end
