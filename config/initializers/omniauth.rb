67de0a89ebe773ce849cdb00511e10ce21d858b6
Matias H. Leidemer <matiasleidemer@gmail.com>
2012-05-01 15:47:56 -0300

    those fb keys should be secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "294613237287030", "a0401f108c5ea18ae8cda324d2f9b1a9", :scope => 'email,offline_access,read_stream', :display => 'popup'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,offline_access,read_stream', :display => 'popup'
 end
